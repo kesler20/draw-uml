290ad872e7cb33d1525bf87173fb0a4a3ef59e12
kesler <uchekesla@gmail.com>
2022-07-09 20:16:24 +0100

    restarted again

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { NavLink } from "react-router-dom";
-import Logo from "../Logo.svg";
+import Logo from "../logo.svg";
 import { useState, useEffect } from "react";
 
 const Navbar = ({
