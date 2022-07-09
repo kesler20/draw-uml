9dce5d03c4417d0c3a251ce49af78750d1fc1d45
kesler <uchekesla@gmail.com>
2022-07-09 18:28:45 +0100

    added navbar and new features

--- src/components/Console.jsx
+++ src/components/Console.jsx
@@ -3,8 +3,15 @@ import ReactFlow, {
   addEdge,
   applyEdgeChanges,
   applyNodeChanges,
+  MiniMap,
+  Controls,
+  Background,
 } from "react-flow-renderer";
+import Navbar from "./Navbar.jsx";
 import UmlDiagram from "./UmlDiagram.jsx";
+
+
+const storageSize = 1000;
 
 const getRandomNumber = (maxNum) => {
   return Math.floor(Math.random() * maxNum);
@@ -18,6 +25,14 @@ const getRandomColor = () => {
   return `rgb(${r}, ${g}, ${b})`;
 };
 
+const loadItemsFromLocalStorage = () => {
+  let storedItems = [];
+  for (let i = 0; i < storageSize; i++) {
+    storedItems.push(localStorage.key(i));
+  }
+  return storedItems.filter((item) => item !== null);
+};
+
 const initialNodes = [
   {
     id: "node-1",
@@ -28,16 +43,7 @@ const initialNodes = [
       gridTable: [{ visibility: "+", signature: "", type: "" }],
       connection: false,
     },
-  },
-  {
-    id: "node-2",
-    type: "umlDiagram",
-    position: { x: 0, y: 0 },
-    data: {
-      color: getRandomColor(),
-      gridTable: [{ visibility: "+", signature: "", type: "" }],
-    },
-  },
+  }
 ];
 
 const initialEdges = [];
@@ -45,6 +51,7 @@ const initialEdges = [];
 const nodeTypes = { umlDiagram: UmlDiagram };
 
 const Console = () => {
+
   const [edges, setEdges] = useState(initialEdges);
   const [nodes, setNodes] = useState(initialNodes);
 
@@ -61,6 +68,32 @@ const Console = () => {
     [setEdges]
   );
 
+  const loadNodesFromLocalStorage = () => {
+    let nodesFromLocalStorage = [];
+    let elementsNodesKeys;
+    let elementsFromLocalStorage = loadItemsFromLocalStorage();
+    elementsNodesKeys = elementsFromLocalStorage.filter((item) =>
+      item.includes("node")
+    );
+    elementsNodesKeys.forEach((nd) => {
+      nodesFromLocalStorage.push(JSON.parse(localStorage.getItem(nd)));
+    });
+    nodesFromLocalStorage.forEach((nds) => {
+      nds.id = `node-${nodes.length + 1}`
+      nodes.push(nds)
+      console.log(nds)
+    })
+    setNodes(nodes)
+  };
+
+  const saveToLocalStorage = () => {
+    nodes.forEach((node) => {
+      localStorage.setItem(node.id, JSON.stringify(node));
+    });
+    console.log("the following nodes have been saved", nodes);
+    alert('Diagram Saved Successfully ☑️')
+  };
+
   const createTable = () => {
     nodes.push({
       id: `node-${nodes.length + 1}`,
@@ -71,77 +104,47 @@ const Console = () => {
         gridTable: [{ visibility: "+", signature: "", type: "" }],
       },
     });
-    setNodes(nodes)
-    console.log(nodes)
+    setNodes(nodes);
+    console.log(nodes);
   };
+
   return (
     <div>
-      <section>
-        <div className="site-info flex-row">
-          <div className="info flex-row">
-            <h1>drawUML</h1>
-            <p>Verison 0.0.1</p>
-          </div>
-          <div></div>
-          <div style={{ color: "blue" }}>
-            <i className="fas fa-save fa-2x" style={{ margin: 40 }}></i>
-            <i className="fas fa-copy fa-2x"></i>
-          </div>
-          <div className="info-btn flex-row">
-            <button className="btn create-table-btn" onClick={createTable}>
-              Create Table
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 15 15"
-                width="12"
-                height="12"
-                style={{ marginLeft: "0.33em" }}
-              >
-                <g
-                  stroke="currentColor"
-                  strokeWidth="1.75"
-                  fill="none"
-                  fillRule="evenodd"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path
-                    d="M4.497 1H3a2 2 0 00-2 2v9a2 2 0 002 2h9a2 2 0 002-2v-1.5h0"
-                    opacity=".6"
-                  ></path>
-                  <path d="M9 1.008L14 1v5M14 1L6 9"></path>
-                </g>
-              </svg>
-            </button>
-            <i
-              className="fa fa-question-circle fa-2x"
-              style={{ color: "blue" }}
-            ></i>
-          </div>
-        </div>
+      <section style={{ margin: "0% 20%" }}>
+        <Navbar
+          onCreateTable={() => createTable()}
+          onDownloadDiagram={() => loadNodesFromLocalStorage()}
+          onSaveDiagram={() => saveToLocalStorage()}
+          onCopyDiagram={() => loadNodesFromLocalStorage()}
+        />
       </section>
 
-      <section>
-        <div className="data grid">
-          <div className="functions-list"></div>
-          <div className="data-model-canvas">
-            <div style={{ width: "100%", height: "100vh" }}>
-              <ReactFlow
-                nodes={nodes}
-                edges={edges}
-                onNodesChange={onNodesChange}
-                onEdgesChange={onEdgesChange}
-                onConnect={onConnect}
-                nodeTypes={nodeTypes}
-                fitView
-              />
-            </div>
+      <section style={{ margin: "0% 2%" }}>
+        <div className="functions-list"></div>
+        <div className="data-model-canvas">
+          <div style={{ width: "100%", height: "85vh" }}>
+            <ReactFlow
+              nodes={nodes}
+              edges={edges}
+              defaultNodes={nodes}
+              defaultEdges={edges}
+              onNodesChange={onNodesChange}
+              onEdgesChange={onEdgesChange}
+              onConnect={onConnect}
+              nodeTypes={nodeTypes}
+            >
+              <Background />
+              <Controls />
+              <MiniMap />
+            </ReactFlow>
           </div>
         </div>
-        <div className="data-model data-model-grid"></div>
       </section>
     </div>
   );
 };
 
 export default Console;
+
+
+
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,193 @@
+import React from "react";
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+import Logo from "../Logo.svg";
+import { useState, useEffect } from "react";
+
+const Navbar = ({
+  onCreateTable,
+  onSaveDiagram,
+  onDownloadDiagram,
+  onCopyDiagram,
+}) => {
+  const [sideBarView, setSideBarView] = useState([]);
+  // true if there is storage data and false if there is not
+  const [storageData, setStorageData] = useState([]);
+
+  const handleStorage = () => {
+    if (!storageData) {
+      localStorage.clear();
+      setStorageData(true);
+    } else {
+      alert("By clicking this button Again you might lose all your files");
+      setStorageData(false);
+    }
+  };
+
+  useEffect(() => {
+    changeBarView();
+  }, []);
+
+  const changeBarView = () => {
+    let currentView = !sideBarView;
+    setSideBarView(currentView);
+  };
+
+  const renderSideBar = (toRender) => {
+    if (toRender) {
+      return (
+        <SideBar style={{ margin: "10px", color: "#e17bef" }}>
+          <div className="avatar" data-tooltip="Save Diagram">
+            <i className="fas fa-save fa-2x" onClick={onSaveDiagram} />
+          </div>
+          <div className="avatar" data-tooltip="Empty Storage">
+            <i className="fas fa-trash fa-2x" onClick={handleStorage}></i>
+          </div>
+          <div className="avatar" data-tooltip="Load Diagram">
+            <i
+              className="fas fa-download fa-2x"
+              onClick={onDownloadDiagram}
+            ></i>
+          </div>
+          <div className="avatar" data-tooltip="Take Notes">
+            <i className="fas fa-pen fa-2x modal-btn"></i>
+          </div>
+          <div className="avatar" data-tooltip="Copy Notes">
+            <i className="fas fa-copy fa-2x" onClick={onCopyDiagram}></i>
+          </div>
+        </SideBar>
+      );
+    } else {
+      return "";
+    }
+  };
+  return (
+    <div>
+      <NavBar>
+        <HamburgerMenu onClick={() => changeBarView()}>
+          <i className="fa fa-bars fa-2x" style={{ color: "grey" }}></i>
+        </HamburgerMenu>
+        <SiteTitle>
+          <img src={Logo} alt="site logo" />
+          <p>drawUML</p>
+        </SiteTitle>
+        <Version>
+          <p>version: {process.env.REACT_APP_VERSION}</p>
+        </Version>
+        <button className="btn create-table-btn" onClick={onCreateTable}>
+          Create Table
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 15 15"
+            width="12"
+            height="12"
+            style={{ marginLeft: "0.33em" }}
+          >
+            <g
+              stroke="currentColor"
+              strokeWidth="1.75"
+              fill="none"
+              fillRule="evenodd"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path
+                d="M4.497 1H3a2 2 0 00-2 2v9a2 2 0 002 2h9a2 2 0 002-2v-1.5h0"
+                opacity=".6"
+              ></path>
+              <path d="M9 1.008L14 1v5M14 1L6 9"></path>
+            </g>
+          </svg>
+        </button>
+      </NavBar>
+      <div />
+      {renderSideBar(sideBarView)}
+    </div>
+  );
+};
+
+const NavBar = styled.nav`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 100%;
+  height: 80px;
+  border: 10px black;
+`;
+
+const HamburgerMenu = styled.div`
+  border: 0.1px solid rgb(186, 202, 183);
+  width: 57px;
+  height: 42px;
+  border-radius: 5px;
+  display: flex;
+  justify-content: space-evenly;
+  flex-direction: column;
+  align-items: center;
+
+  :hover {
+    box-shadow: 1px 1px 1px 10px rgb(245, 255, 229);
+  }
+
+  .hamburger {
+    border-bottom: solid 3px grey;
+    width: 28px;
+  }
+`;
+const SiteTitle = styled.div`
+  font-size: 40px;
+  font-weight: 700;
+  width: 280px;
+  display: flex;
+  justify-content: space-evenly;
+  p {
+    background: linear-gradient(
+      90deg,
+      rgba(0, 0, 255, 1) 0%,
+      rgba(238, 130, 238, 1) 100%
+    );
+    background-repeat: no-repeat;
+    -webkit-text-fill-color: transparent;
+    -webkit-background-clip: text;
+    background-clip: text;
+    font-weight: 900;
+
+    :hover {
+      background-size: 80%;
+      animation: animate 5s infinite;
+    }
+  }
+
+  img {
+    width: 59px;
+    height: 53px;
+    border-radius: 20px;
+  }
+`;
+
+const Version = styled.div`
+  font-size: 28px;
+  font-weight: 400;
+  display: flex;
+  color: #555555;
+  justify-content: space-evenly;
+  width: 180px;
+  align-items: center;
+`;
+
+const SideBar = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 100%;
+
+  i:hover {
+    color: #8448f5;
+  }
+`;
+
+
+export default Navbar;
+
